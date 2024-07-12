4a2102d2b6517cdeb2e767afb3763ea73b28a020
Omar Issa <omarissa@oryxdentalsoftware.com>
2024-07-12 15:51:59 +0200

    added search and category filter

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,6 +1,6 @@
-// import CategoryFilter from '@/components/shared/CategoryFilter';
+import CategoryFilter from "@/components/shared/CategoryFilter";
 import Collection from "@/components/shared/Collection";
-// import Search from '@/components/shared/Search';
+import Search from "@/components/shared/Search";
 import { getAllEvents } from "@/lib/actions/event.actions";
 import { SearchParamProps } from "@/types";
 import { Button } from "@/components/ui/button";
@@ -54,10 +54,10 @@ export default async function Home({ searchParams }: SearchParamProps) {
           Trust by <br /> Thousands of Events
         </h2>
 
-        {/* <div className='flex w-full flex-col gap-5 md:flex-row'>
+        <div className='flex w-full flex-col gap-5 md:flex-row'>
           <Search />
           <CategoryFilter />
-        </div> */}
+        </div>
 
         <Collection
           data={events?.data}
