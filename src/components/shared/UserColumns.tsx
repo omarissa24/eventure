f7259abdd33fa589ae642b2e9df2d4b7ba9286f4
Omar Issa <omarissa@oryxdentalsoftware.com>
2024-08-12 22:46:38 +0300

    added user column sorting + delete modal

--- src/components/shared/UserColumns.tsx
+++ src/components/shared/UserColumns.tsx
@@ -3,6 +3,9 @@
 import { ColumnDef } from "@tanstack/react-table";
 import { User } from "@/types";
 import Image from "next/image";
+import { UserDeleteConfirmation } from "./UserDeleteConfirmation";
+import { ArrowUpDown } from "lucide-react";
+import { Button } from "../ui/button";
 
 export const userColumns: ColumnDef<User>[] = [
   {
@@ -23,23 +26,55 @@ export const userColumns: ColumnDef<User>[] = [
     },
   },
   {
-    accessorKey: "firstName",
-    header: "First Name",
-  },
-  {
-    accessorKey: "lastName",
-    header: "Last Name",
+    accessorKey: "name",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Name
+          <ArrowUpDown className='ml-2 h-4 w-4' />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return `${row.original.firstName} ${row.original.lastName}`;
+    },
   },
   {
     accessorKey: "email",
-    header: "Email",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Email
+          <ArrowUpDown className='ml-2 h-4 w-4' />
+        </Button>
+      );
+    },
   },
   {
     accessorKey: "role",
-    header: "Role",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Role
+          <ArrowUpDown className='ml-2 h-4 w-4' />
+        </Button>
+      );
+    },
   },
   {
     accessorKey: "actions",
     header: "",
+    cell: ({ row }) => {
+      return <UserDeleteConfirmation clerkId={row.original.clerkId} />;
+    },
   },
 ];
