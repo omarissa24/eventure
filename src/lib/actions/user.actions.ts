0e347b3755b3262fe567d1633d86da34944fd37b
Omar Issa <omarissa@oryxdentalsoftware.com>
2024-08-12 23:02:40 +0300

    fixed clerk user deletion

--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -9,6 +9,7 @@ import Event from "@/lib/database/models/event.model";
 import { handleError } from "@/lib/utils";
 
 import { CreateUserParams, UpdateUserParams } from "@/types/index";
+import { clerkClient } from "@clerk/nextjs/server";
 
 export async function createUser(user: CreateUserParams) {
   try {
@@ -89,6 +90,7 @@ export async function deleteUser(clerkId: string) {
 
     // Delete user
     const deletedUser = await User.findByIdAndDelete(userToDelete._id);
+    await clerkClient.users.deleteUser(userToDelete.clerkId);
     revalidatePath("/");
 
     return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null;
